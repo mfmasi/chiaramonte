9c3193107687e7ab9fc8f4584ba35363d1bc6d64
mfmasi <mf.masi@alumno.etec.um.edu.ar>
2026-07-27 08:51:12 -0300

    Espero que esto funcione

--- app/src/main/kotlin/org/example/App.kt
+++ app/src/main/kotlin/org/example/App.kt
@@ -6,7 +6,7 @@ package org.example
 class App {
     val greeting: String
         get() {
-            return "Lorem Ipsum Dolor Sit Amet!"
+            return "Auxilio tengo frío y miedo"
         }
 }
 
